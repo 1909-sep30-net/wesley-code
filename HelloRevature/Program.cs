2440fa88186bd8595b4ac1f31a83d4151a020520
Wesley Tang <wesley.tang25@gmail.com>
2019-10-01 11:49:04 -0500

    add some notes and hello world c#

---
+++ HelloRevature/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HelloRevature
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Revature!");
+        }
+    }
+}
